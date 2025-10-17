5b090e7a0e2fedb3998be7e9c0dd983fa35628c6
John Savoy <savoy.software.solutions@gmail.com>
2025-10-17 11:26:49 -0400

    JS 10.17.25 - Created and styled FAQ Section; need to return and edit q&a's

--- script.js
+++ script.js
@@ -18,3 +18,17 @@ buttons.forEach(button => {
         });
     });
 });
+
+const faqItems = document.querySelectorAll('.faq_item');
+
+faqItems.forEach(item => {
+    const question = item.querySelector('.faq_question');
+
+    question.addEventListener('click', () => {
+        faqItems.forEach(i => {
+            if (i !== item) i.classList.remove('active');
+        });
+
+        item.classList.toggle('active');
+    });
+});
